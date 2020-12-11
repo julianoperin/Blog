be6bfa6ed479dfee96b2770651973e63de6c38b8
‘Juliano <julianoperins@gmail.com>
2020-12-10 22:10:43 -0500

    Finished the navbar

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePost = () => {
+  return (
+    <div className="create-post">
+      <h1>CreatePost</h1>
+    </div>
+  );
+};
+
+export default CreatePost;
